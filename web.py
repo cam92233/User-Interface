f18112b761526b71a6b375527b3466e36bb18a22
Jamal <jsmalls128@live.com>
2018-11-07 19:59:44 -0500

    web update

--- web.py
+++ web.py
@@ -1,31 +1,35 @@
 from mako.template import Template
+from contextlib import closing
 from pprint import pprint
 import mysql.connector
+import cgi
+from cgi import parse_qs, escape
 
 # setup mysql connection information
 mysql_connection_info = {
     'host': 'localhost',
-    'user': 'pj03',
-    'passwd': 'Project3!',
-    'database': 'pj03',
+    'user': 'web',
+    'passwd': 'WebProject1!',
+    'database': 'web',
     'auth_plugin': 'mysql_native_password'
 }
 
-mysql_connection = mysql.connector.connect(**mysql_connection_info)
 
-def query_presidents():
+def query_presidents(mysql_connection):
     mysql_cursor = mysql_connection.cursor(dictionary = True)
-    mysql_cursor.execute("SELECT * FROM presidents")
+    mysql_cursor.execute("SELECT * FROM books")
     presidents = mysql_cursor.fetchall()
     pprint(presidents)
     return presidents
 
 def application(env, start_response):
+    mysql_connection = mysql.connector.connect(**mysql_connection_info)
     start_response('200 OK', [('Content-Type', 'text/html')])
     pprint(env)
-    html_template = Template(filename = 'templates/index.html')
+    html_template = Template(filename = 'templates/home.html')
     html_dict = {
-   #     'presidents': query_presidents()
+       'presidents': query_presidents(mysql_connection)
     } # html_dict
     response = html_template.render(**html_dict)
+    mysql_connection.close()
     return response.encode()
