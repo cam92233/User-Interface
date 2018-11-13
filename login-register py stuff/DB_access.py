f2bd0bad4570b771cda3e4c34bce50ab63a17bf2
trinity <whopointseven@gmail.com>
2018-11-13 11:53:16 -0500

    Add files via upload

---
+++ login-register py stuff/DB_access.py
@@ -0,0 +1,30 @@
+import mysql.connector
+
+# setup mysql connection information
+mysql_connection_info = {
+    'host': '',
+    'user': '',
+    'passwd': '',
+    'database': '',
+    'auth_plugin': 'mysql_native_password'
+}
+
+def update(query):
+    mysql_connection = mysql.connector.connect(**mysql_connection_info)
+    mysql_cursor = mysql_connection.cursor()
+    mysql_cursor.execute(query)
+    mysql_connection.commit()
+    mysql_connection.close()
+    #for inserting new tuples and updating/deleting old ones
+    return "hello?"
+
+def select(query): #for retrieving info
+    mysql_connection = mysql.connector.connect(**mysql_connection_info)
+    mysql_cursor = mysql_connection.cursor()
+    mysql_cursor.execute(query)
+    rows = mysql_cursor.fetchall()
+    numrows = len(rows)
+    mysql_connection.close()
+    return [rows,numrows]
+    
+    
