0c15177a2c01a1de4d4c33aaa134cab1a91a9094
Jamal <jsmalls128@live.com>
2018-11-13 21:49:47 -0500

    button

--- web.py
+++ web.py
@@ -14,13 +14,27 @@ mysql_connection_info = {
     'database': 'web',
     'auth_plugin': 'mysql_native_password'
 }
-
+def verifyLogin(username,password,mysql_connection):
+    mysql_cursor = mysql_connection.cursor(dictionary = True)
+    mysql_cursor.execute("SELECT password, acct_type FROM accounts WHERE username='{}'".format(username))
+    storedPassword = mysql_cursor.fetchall()
+    pprint(storedPassword)
+    if(storedPassword[0].get("password") == password):
+      acctType = storedPassword[0].get("acct_type")
+      return True
+    return False
+  
 def getBookInfo(title,mysql_connection):
     mysql_cursor = mysql_connection.cursor(dictionary = True)
     mysql_cursor.execute("SELECT * FROM books WHERE isbn='{}'".format(title))
     bookInfo = mysql_cursor.fetchall()
-    pprint(bookInfo)
     return bookInfo
+    
+def updateBook(mysql_connection):
+    mysql_cursor = mysql_connection.cursor(dictionary = True)
+    mysql_cursor.execute("UPDATE books SET price=price-1.0 WHERE title='Test Title'")
+    mysql_connection.commit()
+    pprint("UpdateBook")   
 
 def searchHandler(mysql_connection,field,search):
     mysql_cursor = mysql_connection.cursor(dictionary = True)
@@ -30,46 +44,81 @@ def searchHandler(mysql_connection,field,search):
 
 def query_presidents(mysql_connection):
     mysql_cursor = mysql_connection.cursor(dictionary = True)
-    mysql_cursor.execute("SELECT * FROM books")
+    mysql_cursor.execute("SELECT * FROM books ORDER BY title")
     presidents = mysql_cursor.fetchall()
-    pprint(presidents)
     return presidents
 
 def application(env, start_response):
     mysql_connection = mysql.connector.connect(**mysql_connection_info)
     start_response('200 OK', [('Content-Type', 'text/html')])
-    uwsgi.cache_set("name","John")
+    username = "Account"
     qs = parse_qs(env['QUERY_STRING'])
     if len(qs) > 0:
-      searchText = qs.get("search","")
-      if len(searchText) > 0:
-        searchText = str(searchText[0])
-      searchType = qs.get("radiobutton","")
-      if len(searchType) > 0:
-        searchType = str(searchType[0])
-      if not len(searchText) > 0:
-        detailedBook = cgi.escape(str(qs.get("book","")[0]))
-        if len(detailedBook) > 0:
-          html_template = Template(filename = 'templates/detailedTemplate.html')
-          html_dict = {
-             'bookInfo': getBookInfo(detailedBook,mysql_connection)
-          }
-          response = html_template.render(**html_dict)
-          mysql_connection.close()
-          return response.encode()
-      if len(searchText) > 0:
+      action = qs.get("update")
+      action = action[0]
+      action = int(action)
+      if(action == 1):
+        pprint("made one")
+        updateBook(mysql_connection)
         html_template = Template(filename = 'templates/home.html')
         html_dict = {
-         'presidents': searchHandler(mysql_connection,searchType,searchText)
+         'presidents': query_presidents(mysql_connection),
+         'username': username
         } # html_dict
         response = html_template.render(**html_dict)
         mysql_connection.close()
         return response.encode()
-    pprint(env)
+      else:
+        searchText = qs.get("search","")
+        if len(searchText) > 0:
+          searchText = str(searchText[0])
+        searchType = qs.get("radiobutton","")
+        if len(searchType) > 0:
+          searchType = str(searchType[0])
+        if not len(searchText) > 0:
+          detailedBook = qs.get("book","")
+          if(str(detailedBook) != ""):
+            detailedBook = cgi.escape(str(detailedBook[0]))
+          if len(detailedBook) > 0:
+            html_template = Template(filename = 'templates/detailedTemplate.html')
+            html_dict = {
+               'bookInfo': getBookInfo(detailedBook,mysql_connection)
+            }
+            response = html_template.render(**html_dict)
+            mysql_connection.close()
+            return response.encode()
+        if len(searchText) > 0:
+          html_template = Template(filename = 'templates/home.html')
+          html_dict = {
+           'presidents': searchHandler(mysql_connection,searchType,searchText),
+           'username': username
+          } # html_dict
+          response = html_template.render(**html_dict)
+          mysql_connection.close()
+          return response.encode()
+    if env['REQUEST_METHOD'] == 'POST':
+      content_length = int(env.get('CONTENT_LENGTH', 0))
+      post_data = env['wsgi.input'].read(content_length)
+      qs = parse_qs(post_data)
+      pprint(qs)
+      len(qs < 5)
+        username = qs.get("username".encode(),"")[0].decode('ASCII')
+        password = qs.get("password".encode(),"")[0].decode('ASCII')
+        if(verifyLogin(username,password,mysql_connection)):
+          html_template = Template(filename = 'templates/home.html')
+          html_dict = {
+           'presidents': query_presidents(mysql_connection),
+           'username': username
+          } # html_dict
+          response = html_template.render(**html_dict)
+          mysql_connection.close()
+          return response.encode()  
+          
     html_template = Template(filename = 'templates/home.html')
+    username = "Account"
     html_dict = {
        'presidents': query_presidents(mysql_connection),
-       'acct_name': uwsgi.cache_get("name").decode('ASCII')
+       'username': username
     } # html_dict
     response = html_template.render(**html_dict)
     mysql_connection.close()
